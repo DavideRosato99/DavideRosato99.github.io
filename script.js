98c38a23edbac120f4fad2141fc2d8093c635343
Davide Rosato <73779437+DavideRosato99@users.noreply.github.com>
2023-12-10 19:16:25 +0100

    ada

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
-// Riferimento al contenitore del calendario
 const calendarContainer = document.querySelector('.calendar');
-
-// Imposta la data di inizio e fine per il calendario dell'Avvento (11 dicembre - 25 dicembre)
 const startDate = 11;
 const endDate = 25;
 
-// Genera i giorni del calendario dal 11 dicembre al 25 dicembre
 for (let day = startDate; day <= endDate; day++) {
   const dayElement = document.createElement('div');
   dayElement.classList.add('day');
   dayElement.textContent = day;
 
-  // Aggiungi un'azione al clic del giorno
   dayElement.addEventListener('click', () => {
     alert(`Hai aperto il giorno ${day} del calendario dell'Avvento!`);
-    // Qui potresti aggiungere ulteriori azioni quando un giorno viene aperto
   });
 
+  // Imposta variabili CSS personalizzate per l'animazione di ciascuna cella
+  dayElement.style.setProperty('--delay', Math.random() * 2);
+
   calendarContainer.appendChild(dayElement);
+}
+
+// Funzione per calcolare la durata dell'animazione in base alla velocità
+function calculateAnimationDuration(velocity) {
+  const minVelocity = 1; // Velocità minima
+  const maxVelocity = 10; // Velocità massima
+  const minDuration = 5; // Durata minima
+  const maxDuration = 20; // Durata massima
+
+  return minDuration + ((velocity - minVelocity) / (maxVelocity - minVelocity)) * (maxDuration - minDuration);
 }
 
 // Genera fiocchi di neve
 const snowflakesContainer = document.querySelector('.snowflakes');
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 2000; i++) {
+  const velocity = Math.random() * 10 + 1; // Genera velocità casuale
+  const animationDuration = calculateAnimationDuration(velocity); // Calcola la durata dell'animazione
+
   const snowflake = document.createElement('div');
   snowflake.classList.add('snowflake');
   snowflake.style.left = `${Math.random() * 100}vw`; // Posizione casuale orizzontale
-  snowflake.style.animationDuration = `${Math.random() * 10 + 2}s`; // Durata animazione casuale
+  snowflake.style.animationDuration = `${animationDuration}s`; // Imposta la durata calcolata
   snowflake.style.animationDelay = `${Math.random() * 10}s`; // Ritardo casuale per inizio animazione
+
+  const size = Math.ceil(10/ velocity * 1.5); // Dimensioni proporzionali alla velocità
+  snowflake.style.width = `${size}px`;
+  snowflake.style.height = `${size}px`;
+
   snowflakesContainer.appendChild(snowflake);
 }
