9ef4136a71c587fed44173bcf592f5ea7a5b3cf5
Davide Rosato <73779437+DavideRosato99@users.noreply.github.com>
2023-12-10 19:46:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-// JavaScript
+const today = 12;
+const openedDays = JSON.parse(localStorage.getItem('openedDays')) || [];
+
 const calendarContainer = document.querySelector('.calendar');
 const startDate = 11;
 const endDate = 25;
-
-const currentDate = new Date();
-const today = 12; // Ottiene il giorno corrente
-
-// Controlla se è presente un valore salvato nello storage
-let openedDays = JSON.parse(localStorage.getItem('openedDays')) || {};
 
 for (let day = startDate; day <= endDate; day++) {
   const dayElement = document.createElement('div');
   dayElement.classList.add('day');
   dayElement.textContent = day;
 
-  if (openedDays[day]) {
-    dayElement.classList.add('clicked');
+  if (openedDays.includes(day)) {
+    dayElement.classList.add('opened');
   }
 
   dayElement.addEventListener('click', () => {
-    if (day !== today) {
-      alert(`Sei stupida!`);
-      return;
+    if (day === today) {
+      if (!openedDays.includes(day)) {
+        openedDays.push(day);
+        localStorage.setItem('openedDays', JSON.stringify(openedDays));
+        dayElement.classList.add('opened');
+        alert(`Hai aperto il giorno ${day} del calendario dell'Avvento!`);
+      } else {
+        alert(`Hai già aperto il giorno ${day} del calendario dell'Avvento!`);
+      }
+    } else {
+      alert('Sei stupida!');
     }
-
-    dayElement.classList.add('clicked');
-    openedDays[day] = true;
-    localStorage.setItem('openedDays', JSON.stringify(openedDays));
-    alert(`Hai aperto il giorno ${day} del calendario dell'Avvento!`);
   });
 
   // Imposta variabili CSS personalizzate per l'animazione di ciascuna cella
@@ -35,10 +34,6 @@ for (let day = startDate; day <= endDate; day++) {
 
   calendarContainer.appendChild(dayElement);
 }
-localStorage.removeItem('openedDays');
-
-// O se si desidera rimuovere tutti i dati dal localStorage
-localStorage.clear();
 
 // Funzione per calcolare la durata dell'animazione in base alla velocità
 function calculateAnimationDuration(velocity) {
@@ -63,9 +58,21 @@ for (let i = 0; i < 2000; i++) {
   snowflake.style.animationDuration = `${animationDuration}s`; // Imposta la durata calcolata
   snowflake.style.animationDelay = `${Math.random() * 10}s`; // Ritardo casuale per inizio animazione
 
-  const size = Math.ceil(10/ velocity * 1.5); // Dimensioni proporzionali alla velocità
+  const size = Math.ceil(10 / velocity * 1.5); // Dimensioni proporzionali alla velocità
   snowflake.style.width = `${size}px`;
   snowflake.style.height = `${size}px`;
 
   snowflakesContainer.appendChild(snowflake);
 }
+
+// Resetta tutto
+function resetCalendar() {
+  localStorage.removeItem('openedDays');
+  const openedElements = document.querySelectorAll('.day.opened');
+  openedElements.forEach((element) => {
+    element.classList.remove('opened');
+  });
+}
+
+// Per esempio, per resettare tutto:
+// resetCalendar();
